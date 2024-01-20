459df7cf40cec632b3ab9cce98b03566a73484e9
Давид Мкртчян <dovik03@mail.ru>
2024-01-20 14:47:30 +0300

    sssss

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
       <div className={styles.container}>
         <div className={styles.header__logo}>
           <Link to={""} style={{ textDecoration: "none", color: "black" }}>
-            <div>Преподаватели</div>
+            <div>Наши преподаватели</div>
           </Link>
         </div>
 
